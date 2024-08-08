a3160c22f0329d133f0b66b11faee752f2ba4911
lior <liorgishry@microsoft.com>
2024-08-08 16:27:02 +0300

    more

--- 14_tag/operands.c
+++ 14_tag/operands.c
@@ -3,7 +3,8 @@
 #include <stdbool.h> // Ensure this is included for bool
 
 // Array of valid operands
-static const char* valid_regiters[] = { "r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7" };
+static const char* valid_regiters[] = { "r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7","*r0", "*r1", "*r2", "*r3", "*r4", "*r5", "*r6", "*r7" };
+static const char* valid_regiters_2[] = { "*r0", "*r1", "*r2", "*r3", "*r4", "*r5", "*r6", "*r7" };
 static const size_t num_registers = sizeof(valid_regiters) / sizeof(valid_regiters[0]);
 
 /**
@@ -19,3 +20,36 @@ bool is_valid_regiter(const char* operand) {
 	}
 	return false;
 }
+
+bool is_valid_regiter_2(const char* operand) {
+	for (size_t i = 0; i < num_registers; ++i) {
+		if (strcmp(valid_regiters_2[i], operand) == 0) {
+			return true;
+		}
+	}
+	return false;
+}
+Operand_Type get_operand_type(const char* operand) {
+	if (is_valid_regiter(operand)) {
+		return Register;
+	}
+	else if (strcmp(operand[0], "#") == 0) {
+
+		return Number;
+	}
+	return Symbol;
+}
+
+AddressingType get_addressing_type(const char* operand) {
+	if (is_valid_regiter(operand)) {
+		return DirectRegister;
+	}
+	else if (is_valid_regiter_2(operand)) {
+		return IndirectRegister;
+	}
+	else if (strcmp(operand[0], "#") == 0) {
+		return Immediate;
+	}
+	return Direct;
+}
+
--- 14_tag/operands.h
+++ 14_tag/operands.h
@@ -6,4 +6,21 @@
 // Function declaration
 bool is_valid_regiter(const char* operand);
 
+typedef  enum Operand_Type {
+	Register,
+	Symbol,
+	Number
+} Operand_Type;
+
+typedef enum AddressingType {
+	Direct,
+	Immediate,
+	DirectRegister,
+	IndirectRegister
+} AddressingType;
+
+Operand_Type get_operand_type(const char* operand);
+AddressingType get_addressing_type(const char* operand);
+
+
 #endif // OPERANDS_H
